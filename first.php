97697d19b884061d72f8babc42021e5a01c92754
Zspindrift <Zspindrift@163.com>
2018-03-28 16:03:09 +0800

    第一个Php

---
+++ first.php
@@ -0,0 +1,60 @@
+<?php
+//    $num = 555;
+//    echo $num;
+//    echo "<br/>";
+
+//    echo "123&nbsp;456";
+//    echo "<br/>";
+//    echo "中国";
+//
+//    echo "<script>location='form.php'</script>"
+?>
+
+<!--<meta charset="UTF-8">-->
+<!--<form action="first.php" method="post">-->
+<!--    数据：<input type="text" name="num">-->
+<!--    <input type="submit" name="sub" value="提交">-->
+<!--</form>-->
+
+<!--<p>啊啊啊啊</p>-->
+<!---->
+<?php
+//    for($i=0; $i<10; $i++){
+//        echo $i;
+//    }
+//?>
+<!---->
+<!--<a href="#">baidu</a>-->
+<!---->
+<?php
+//    $name=123;
+//    echo 456;
+//    die();
+//    var_dump($name);
+//    echo 999;
+
+//    $arr = array(1,2,3,4,5);
+//    echo "<pre>";
+//    print_r($arr);
+//    echo "</pre>";
+
+$arr = array(1,2,3,4,5);
+for($i=0; $i<count($arr); $i++){
+    echo $i."<br/>";
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
